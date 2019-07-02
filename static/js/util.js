cc43d6240c672c4dbc5fba2d658145999fc95c71
zqw <zqwde2006@163.com>
2019-07-02 21:32:32 +0800

    'page'

--- static/js/util.js
+++ static/js/util.js
@@ -59,3 +59,37 @@ function recommend (eleWrap, sEle) {
 function loading () {
     $('.loading').remove()
 }
+
+
+
+// $(function() {
+//   var nowpage=0;
+//   var lock=true;
+//   function donghua(){
+//     $('.containter').animate({'top': -100*nowpage+'%'}, 500);
+//     $('.page').eq(nowpage).addClass('current').siblings().removeClass('current');
+//     $('.dian ul li').eq(nowpage).addClass('da').siblings().removeClass('da');
+//     $('.nav ul li').eq(nowpage).addClass('bg').siblings().removeClass('bg');
+//   }
+//   $(document).mousewheel(function (event,delta) {
+//     if(lock){ 
+//       nowpage=nowpage-delta;
+//       if(nowpage<0){nowpage=0;}
+//       if(nowpage>7){nowpage=7;}
+//        donghua();
+//       lock=false;
+//       setTimeout(function  () {
+//         // body...
+//         lock=true;
+//       },1000);
+//     }
+//   })
+//   $('.nav ul li').click(function(event) {
+//     nowpage=$(this).index();
+//     if(nowpage<=7){donghua();}
+//   });
+//   $('.dian ul li').click(function(event) {
+//     nowpage=$(this).index();
+//     donghua();
+//   });
+// });
